bb3d5423a9dc57f5985247b2b23b11893d732be9
Joel Ward <jmward@gmail.com>
2015-09-30 16:52:30 -0700

    add requirements.txt

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@ git clone https://gist.github.com/val314159/6cef2f7df7355d681335
 git clone https://gist.github.com/val314159/7b09f27b5a858bcb7f1e
 git clone https://gist.github.com/val314159/839137f062d43226538b
 git clone https://gist.github.com/val314159/175869ef985012b1d966
+git clone https://gist.github.com/val314159/97eb37b0bd79941e7556
+virtualenv env
+. env/bin/activate
+pip install -r requirements.txt
