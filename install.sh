087d078a18749880c5e8f1257135f578e281a6d0
Joel Ward <jmward@gmail.com>
2015-09-30 15:47:44 -0700

    first cut

---
+++ install.sh
@@ -0,0 +1,4 @@
+git clone https://gist.github.com/val314159/816abfa3579b1b58e373
+git clone https://gist.github.com/val314159/6cef2f7df7355d681335
+git clone https://gist.github.com/val314159/7b09f27b5a858bcb7f1e
+git clone https://gist.github.com/val314159/839137f062d43226538b
