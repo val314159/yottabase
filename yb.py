c9bcbbc9ff7dfee4767220c8d61abb5a759a1ba6
Joel Ward <jmward@gmail.com>
2015-10-01 09:26:09 -0700

    make importwork with python3

--- yb.py
+++ yb.py
@@ -1,10 +1,11 @@
-import yotta
-from yotta.yottapubsub_client import YottaPubSubClient
+#!/usr/bin/env python
+from gevent import monkey; monkey.patch_all()
+from yotta import yottapubsub_client
+YottaPubSubClient=yottapubsub_client.YottaPubSubClient
 
 def onpub(a,b,c,d):
     print("ONPUB", repr((a,b,c,d)))
     pass
 
-ps = YottaPubSubClient("ws://localhost:9028/ws")
-ps.sub( [ 'A' ,'B' ] )
-ps.loop(callback=onpub)
+ps = YottaPubSubClient("ws://localhost:9028/ws").sub(
+    [ 'A' ,'B' ] ).loop(callback=onpub)
